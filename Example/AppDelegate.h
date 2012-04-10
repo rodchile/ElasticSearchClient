1e8f873a5b8f59f425e31933603b2ad4e0144d39
Pulkit Singhal <pulkitsinghal@gmail.com>
2012-04-10 09:29:03 -0500

    Initial Commit

---
+++ Example/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Pulkit Singhal on 4/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
