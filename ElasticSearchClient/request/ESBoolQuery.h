c22f759f2998d111ccf05c4bfb7004af6f95fb5a
Pulkit Singhal <pulkitsinghal@gmail.com>
2012-04-17 10:48:00 -0500

    added the construct to help build the bool query for ES

--- ElasticSearchClient/request/ESBoolQuery.h
+++ ElasticSearchClient/request/ESBoolQuery.h
@@ -6,8 +6,16 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <RestKit/RestKit.h>
 
 @interface ESBoolQuery : NSObject
+
+@property (nonatomic, strong) NSArray*  must;
+@property (nonatomic, strong) NSArray*  must_not;
+@property (nonatomic, strong) NSArray*  should;
+@property (nonatomic, strong) NSNumber* minimum_number_should_match;
+@property (nonatomic, strong) NSNumber* boost;
+
++ (RKObjectMapping*) getObjectMapping;
 
 @end
--- ElasticSearchClient/request/ESBoolQuery.m
+++ ElasticSearchClient/request/ESBoolQuery.m
@@ -7,7 +7,35 @@
 //
 
 #import "ESBoolQuery.h"
+#import "ESWrapper.h"
 
 @implementation ESBoolQuery
+
+@synthesize must, must_not, should, minimum_number_should_match, boost;
+
++ (RKObjectMapping *) getObjectMapping
+{
+    RKObjectMapping* objectMapping = [RKObjectMapping mappingForClass:[ESBoolQuery class]];
+    //objectMapping.forceCollectionMapping = YES;
+
+    [objectMapping mapAttributes:
+     @"minimum_number_should_match",
+     @"boost",
+     nil];
+
+    [objectMapping mapKeyPath:@"must"
+               toRelationship:@"must"
+                  withMapping:[ESWrapper getObjectMapping]];
+
+    [objectMapping mapKeyPath:@"must_not"
+               toRelationship:@"must_not"
+                  withMapping:[ESWrapper getObjectMapping]];
+
+    [objectMapping mapKeyPath:@"should"
+               toRelationship:@"should"
+                  withMapping:[ESWrapper getObjectMapping]];
+
+    return objectMapping;
+}
 
 @end
